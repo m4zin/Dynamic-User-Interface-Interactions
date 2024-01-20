56c8a3971a4e7d764dfbe12ebd9c4afb09fedc1c
m4zin <mazin.sherif@gmail.com>
2024-01-20 22:06:09 +0400

    image slider remain

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
 import '../styles/styles.css'
+// import { dropDown } from './dropdown'
+
+(function() {
+    // For mobile menu
+    let mobDropDownItems = document.querySelector('.hamburger-menu-items');
+    let mobDropDownBtn = document.querySelector('.hamburger-menu-btn');
+
+    mobDropDownBtn.addEventListener('click', () => {
+        mobDropDownItems.style.display = (mobDropDownItems.style.display === 'none' || mobDropDownItems.style.display === '') ? 'flex' : 'none';
+    });
+})();
