1ba8a7141c9adf529cba11663097ddf38b59cc0e
m4zin <mazin.sherif@gmail.com>
2024-01-21 17:55:37 +0400

    mobile menu and image slider remains.

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,28 +1,24 @@
 const dropdown = (function () {
+  function displayDropdown (btn, list, animation) {
+    // When mouse is clicks dropdown button.
+    btn.addEventListener('click', () => {
+      list.classList.toggle(animation)
+    })
 
-    function displayDropdown(btn, list, animation) {
-
-        // When mouse is hovered over button.
-        btn.addEventListener('click', () => {
-            list.classList.toggle(animation);
-        })
-
-        // Close dropdown when clicking outside
-        window.addEventListener('click', (e) => {
-            // Untoggles the dropdown if clicked anywhere else on window.
-            if (list.classList.contains(animation) &&
+    // Close dropdown when clicking outside of relevant btn.
+    window.addEventListener('click', (e) => {
+      // Untoggles the dropdown if clicked anywhere else on window.
+      if (list.classList.contains(animation) &&
                 !btn.contains(e.target) &&
                 !list.contains(e.target)) {
-                list.classList.remove(animation);
-            }
-        });
+        list.classList.remove(animation)
+      }
+    })
+  }
 
-    }
-
-    return {
-        displayDropdown
-    }
-
+  return {
+    displayDropdown
+  }
 })()
 
 export { dropdown }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,25 @@
-import '../styles/styles.css'
+import '../styles/navbar.css'
 import { dropdown } from './dropdown'
 
-(function () {
+function runDropDown() {
+  // Navbar dropdown stuff.
 
-    // dropping down on hover of navbar button services.
-    const servicesBtn = document.querySelector('.services-btn')
-    const servicesList = document.querySelector('.services-dropdown')
+  // dropping down on click of navbar button services.
+  const servicesBtn = document.querySelector('.services-btn')
+  const servicesList = document.querySelector('.services-dropdown')
 
-    dropdown.displayDropdown(servicesBtn, servicesList, 'show-dropdown')
+  // dropping down on click of item four button.
+  const itemThreeBtn = document.querySelector('.item-three-btn')
+  const itemThreeList = document.querySelector('.item-three-dropdown')
+  const itemFourBtn = document.querySelector('.item-four-btn')
+  const itemFourList = document.querySelector('.item-four-dropdown')
+
+  dropdown.displayDropdown(itemThreeBtn, itemThreeList, 'show-dropdown')
+  dropdown.displayDropdown(itemFourBtn, itemFourList, 'show-dropdown')
+  dropdown.displayDropdown(servicesBtn, servicesList, 'show-dropdown')
+}
+
+(function() {
+  runDropDown()
 })()
-
 
